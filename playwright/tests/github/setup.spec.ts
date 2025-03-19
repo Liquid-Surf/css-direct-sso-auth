ff29ec3dffa7092f827a16ddf3f5fcb0a57e5295
thhck <thhck@proton.me>
2025-03-19 23:43:57 +0100

    client: don't use DCR + env PORT variable

--- playwright/tests/github/setup.spec.ts
+++ playwright/tests/github/setup.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from '@playwright/test';
+export const clientUrl = process.env.CLIENT_URL || 'http://localhost:1234';
+
 
 const authFile = './playwright/tests/browser_state/browser_state_github_session.json';
 
 test('Google SSO E2E Test - logs in via Google and loads a file', async ({ page }) => {
-  await page.goto('http://localhost:1234');
+  await page.goto(clientUrl);
 
 
   await page.click('button:has-text("Login with github")');
   await page.waitForNavigation({ timeout: 10000, url: /github\.com/ });
 
-  await page.waitForNavigation({ url: /localhost:1234/ });
+  await page.waitForNavigation({ url: new RegExp(clientUrl) });
   await page.context().storageState({ path: authFile });
 });
